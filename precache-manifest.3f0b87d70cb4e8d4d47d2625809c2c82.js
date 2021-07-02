20a9709c7371171191a29f777337c4788f161e9c
Benjamin Cañas <benjamincanas1@gmail.com>
2021-07-02 16:28:41 -0300

    Updates

---
+++ precache-manifest.3f0b87d70cb4e8d4d47d2625809c2c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca567141e5aeb38aeccabe057e68b10",
+    "url": "/questions-machine/index.html"
+  },
+  {
+    "revision": "92142c292a9ae7e7b15e",
+    "url": "/questions-machine/static/css/main.4abc507c.chunk.css"
+  },
+  {
+    "revision": "f5b5cec432beb8ba812f",
+    "url": "/questions-machine/static/js/2.06c7f987.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/questions-machine/static/js/2.06c7f987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92142c292a9ae7e7b15e",
+    "url": "/questions-machine/static/js/main.ddfc198a.chunk.js"
+  },
+  {
+    "revision": "2c641e3be3340c094428",
+    "url": "/questions-machine/static/js/runtime-main.46fb714a.js"
+  }
+]);
